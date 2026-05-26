83b8bcbc813a452d4d7d9bd0c304c0da07e866e2
Developer <dev@api-flow.local>
2026-05-26 18:10:14 +0800

    fix: 1px snap grid

--- src/components/FlowEditor/SequenceEditor.jsx
+++ src/components/FlowEditor/SequenceEditor.jsx
@@ -280,7 +280,7 @@ export default function SequenceEditor() {
             nodeTypes={nodeTypes}
             fitView fitViewOptions={{ padding: 0.3 }}
             zoomOnScroll={false} panOnScroll={false}
-            snapToGrid={true} snapGrid={[5, 5]}
+            snapToGrid={true} snapGrid={[1, 1]}
             deleteKeyCode={['Backspace', 'Delete']}
             multiSelectionKeyCode="Shift"
           ><Controls /><Background /><MiniMap /></ReactFlow>
